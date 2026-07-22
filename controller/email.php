72e66c431cb039f4377181648e448631fd8e24cf
Marcios <marciossilva.dev@gmail.com>
2026-07-21 22:42:10 -0300

    ajustes

--- controller/email.php
+++ controller/email.php
@@ -3,7 +3,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../dotenv.php';
+carregarEnv(__DIR__ . '/../.env');
 
 function enviarEmailVerificacao($email, $token) {
 
@@ -12,23 +14,26 @@ function enviarEmailVerificacao($email, $token) {
     try {
 
         $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
+        $mail->Host       = $_ENV['MAIL_HOST'] ?? 'smtp.hostinger.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'marciosunico18@gmail.com';
-        $mail->Password   = 'ofzw rdgs egfk ytly';
-        $mail->SMTPSecure = 'tls';
-        $mail->Port       = 587;
+        $mail->Username   = $_ENV['MAIL_USERNAME'] ?? 'adm@zaldemy.com';
+        $mail->Password   = $_ENV['MAIL_PASSWORD'] ?? '';
+        $mail->SMTPSecure = $_ENV['MAIL_ENCRYPTION'] ?? 'ssl';
+        $mail->Port       = $_ENV['MAIL_PORT'] ?? 465;
 
-        $mail->setFrom('marciosunico18@gmail.com', 'Memly');
+        $fromEmail = $_ENV['MAIL_FROM_ADDRESS'] ?? 'adm@zaldemy.com';
+        $fromName  = $_ENV['MAIL_FROM_NAME'] ?? 'Zaldemy';
+        $appUrl    = rtrim($_ENV['APP_URL'] ?? 'https://zaldemy.com', '/');
+
+        $mail->setFrom($fromEmail, $fromName);
         $mail->addAddress($email);
 
-        $link = "https://memly-jijk.vercel.app/emailverificado?token=$token";
-
+        $link = "$appUrl/emailverificado?token=$token";
 
         $mail->isHTML(true);
-        $mail->Subject = 'Confirme seu email';
+        $mail->Subject = 'Confirme seu email - Zaldemy';
         $mail->Body    = "
-            <h2>Confirme sua conta</h2>
+            <h2>Confirme sua conta Zaldemy</h2>
             <p>Clique no link abaixo para verificar seu email:</p>
             <a href='$link'>$link</a>
         ";
